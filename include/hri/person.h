cd5290d956ca0dc5b7d80e31e3f7a602e584e71e
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-02-07 17:31:28 +0100

    add/update BSD license

--- include/hri/person.h
+++ include/hri/person.h
@@ -1,3 +1,12 @@
+// Copyright 2022 PAL Robotics S.L.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
 //      notice, this list of conditions and the following disclaimer in the
 //      documentation and/or other materials provided with the distribution.
 //
@@ -16,7 +25,6 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-
 
 #ifndef HRI_PERSON_H
 #define HRI_PERSON_H
